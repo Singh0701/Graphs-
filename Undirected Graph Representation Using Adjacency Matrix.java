cf053e45a2aadeba4ff7d3e13a70101926790736
Devender Singh <58650049+Singh0701@users.noreply.github.com>
2022-12-03 20:28:33 +0530

    Create Undirected Graph Representation Using Adjacency Matrix.java

---
+++ Undirected Graph Representation Using Adjacency Matrix.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+//Undirected Graph Representation using Adjacency Matrix.
+public class GraphRepresentation {
+    public static void main(String[] agrs) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number of nodes: ");
+        int numberOfNodes = sc.nextInt();
+        System.out.println("Enter the number of edges: ");
+        int numberOfEdges = sc.nextInt();
+
+        int[][] adjacencyMatrix = new int[numberOfNodes][numberOfNodes];
+
+        System.out.println("Enter the edges: ");
+        for(int i = 0; i < numberOfEdges; i++) {
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+            adjacencyMatrix[u][v] = 1;
+            adjacencyMatrix[v][u] = 1;
+        }
+
+        for(int[] row: adjacencyMatrix) {
+            for(int item: row) {
+                System.out.print(item+ " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
